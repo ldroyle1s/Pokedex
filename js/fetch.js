c13699b5fc9fcf03c5f132a78b212d5bd99e45a2
LDRoyle1s <LDRoyle1s@capetigers.com>
2026-05-06 10:09:27 -0500

    Finished!

--- js/fetch.js
+++ js/fetch.js
@@ -17,9 +17,33 @@ async function fetchData(){
         const pokemonHeight = (data.height / 3.048).toFixed(2);
         const pokemonWeight = (data.weight / 4.5359237).toFixed(2);
 
+        //the six stats
+        const pokemonHp = data.stats[0].base_stat;
+        const pokemonAtk =  data.stats[1].base_stat;
+        const pokemonDef =  data.stats[2].base_stat;
+        const pokemonSpAtk = data.stats[3].base_stat;
+        const pokemonSpDef = data.stats[4].base_stat;
+        const pokemonSpeed = data.stats[5].base_stat;
+
+        const HpElement = document.getElementById("pokemonHp");
+        const AtkElement = document.getElementById("pokemonAtk");
+        const DefElement = document.getElementById("pokemonDef");
+        const SpAtkElement = document.getElementById("pokemonSpAtk");
+        const SpDefElement = document.getElementById("pokemonSpDef");
+        const SpeedElement = document.getElementById("pokemonSpeed");
+        
+        document.getElementById("pokemonHp").innerHTML = "Hp <br />" + pokemonHp;
+        document.getElementById("pokemonAtk").innerHTML = "Atk <br />" + pokemonAtk;
+        document.getElementById("pokemonDef").innerHTML = "Def <br />" + pokemonDef;
+        document.getElementById("pokemonSpAtk").innerHTML = "Sp Atk <br />" + pokemonSpAtk;
+        document.getElementById("pokemonSpDef").innerHTML = "Sp Def <br />" + pokemonSpDef;
+        document.getElementById("pokemonSpeed").innerHTML = "Speed <br />" + pokemonSpeed;
+
+        //Height and Width
         document.getElementById("pokemonHeight").innerText = pokemonHeight + " Feet";
         document.getElementById("pokemonWeight").innerText = pokemonWeight +" Pounds";
 
+        //Image and audio
         audioElement.src = pokemonCry;
         audioElement.style.display ="block";
         imgElement.src = pokemonSprite;
